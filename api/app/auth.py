57cfc3d57809f302f2b5b34abe898f730d027a4e
sparklyjefff <sparklyjeff@gmail.com>
2022-10-21 20:02:15 -0700

    Created auth

---
+++ api/app/auth.py
@@ -0,0 +1,41 @@
+from app import app
+from flask import request, jsonify, sqlite3
+
+testAccounts = [
+    {
+        "user" : "sparkles",
+        "passw" : "password"
+    },
+    {
+        "user" : "seclab",
+        "passw" : "seclab"
+    }
+]
+
+
+
+@app.get("api/auth")
+def auth():
+    #check if user and pass are passed correctly 
+    if "user" not in request.args or "passw" not in request.args:
+        return "Error: missing username or password"
+
+    #parse request
+    user = request.args.get('user')
+    passw = request.args.get('passw')
+    to_filter = [user]
+
+    # connect to db
+    query = "SELECT * FROM accounts WHERE user=?;"
+    conn = sqlite3.connect('example.db')
+    conn.row_factory = dict_factory
+    curs = conn.cursor()
+
+    res = curr.execute(query, to_filter).fetchall()
+
+    if res['passw'] == passw:
+        res['auth'] = True 
+    else:
+        res['auth'] = False 
+
+    return jsonify(res)

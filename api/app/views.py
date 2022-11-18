389fdc70c6ae13b7305cec0476ffb98e12cf70ca
Daryl Ou <darylou@ucsb.edu>
2022-11-17 16:58:51 -0800

    completed flask api

--- api/app/views.py
+++ api/app/views.py
@@ -1,22 +1,51 @@
 from app import app
 from flask import abort, request
+import sqlite3
 
 @app.get("/api/paste")
 def get_paste():
-    # user = request.form['user']
+    #username = request.form['username']
+    username = 'daryl'
 
-    # TODO: Query database container for paste
-    title = "My Paste"
-    body = "This is my paste."
-    
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+
+    res = cur.execute("SELECT * FROM pastes WHERE username = '" + username + "'")
+    body = res.fetchall()[0][1]
+    con.close()
     return {
-        "title": title,
         "body": body,
     }
 
 
 @app.post("/api/paste")
 def post_paste():
+    #username = request.form['username']
+    #body = request.form['body']
+    username = 'daryl'
+    body = 'this is my second paste'
+
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+
+    res = cur.execute("SELECT * FROM pastes WHERE username = '" + username + "'")
+
+    
+    if len(res.fetchall()) == 0:
+        cur.execute("INSERT INTO pastes ('username','paste') VALUES ('" + username + "','" + body + "')")
+    else:
+        cur.execute("UPDATE pastes SET paste = '" + body + "' WHERE username = '" + username + "'")
+    
+    con.commit()
+
+    data = cur.execute("SELECT * FROM pastes")
+    for row in data:
+        print(row);
+
+
+
+    con.close()
+    
     return {
         "stub": "stub",
     }

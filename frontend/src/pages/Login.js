990959da55eadaca666dcbe6b6c24713fa0f1055
Kelly Yan <yan.kelly11@gmail.com>
2023-02-06 15:34:08 -0800

    ky - added async await for account register

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/tabs'
+import axios from 'axios'
 
 function Login() {
     const [username, setUsername] = useState("")
@@ -13,14 +14,26 @@ function Login() {
         setPassword(e.target.value)
     }
 
-    function submitLogin() {
+    function onLogin() {
         // Do something here
         // Show message if there's a login issue
+
     }
-    
-    function submitRegister() {
-        // Register for an account
-    }
+
+    const onRegister = async (e) => {
+        e.preventDefault()
+        const post = { 
+            'user': username,  
+            'passw': password
+        }
+        try {
+          const res = await axios.post('http://127.0.0.1:3030/auth/create', post)
+          console.log(res.data)
+          console.log("success")
+        } catch (e) {
+          alert(e)
+        }
+      }
     
     return (
         <div className='login_container'>
@@ -40,7 +53,7 @@ function Login() {
                 <p>Password</p>
                 <input id='password' onChange={handlePasswordChange}/>
                 <br />
-                <button className='btn' onClick={submitLogin}>Submit</button>
+                <button className='btn' onClick={onLogin}>Submit</button>
                 </div>
                 </TabPanel>
 
@@ -51,7 +64,7 @@ function Login() {
                 <p>Password</p>
                 <input id='password' onChange={handlePasswordChange}/>
                 <br />
-                <button className='btn' onClick={submitRegister}>Submit</button>
+                <button className='btn' onClick={onRegister}>Submit</button>
                 </div>
                 </TabPanel>
             </TabPanels>
